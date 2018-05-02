fc3114a066d47195c001652761418bef3fa87372
David Markel <davidgmarkel@gmail.com>
2018-05-02 18:45:45 +0000

    automatically backed up by learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,9 +1,9 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
 
-  def change 
+  def change
     create_table :genres do |t|
-      t.string :name 
-    end 
+      t.string :name
+    end
   end
   
 end
